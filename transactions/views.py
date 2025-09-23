52df923844446f8c7ad7f26d9c1bcbf54da4a601
fungusAcademy <grivus12@yandex.ru>
2025-09-23 20:04:21 +1000

    FIx: form error when adding transaction
    Add transaction form now oppens transactions list after adding transaction.
    Transaction is now bound to current user when adding transactions.

--- transactions/views.py
+++ transactions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Transaction
 from .forms import TransactionForm
 
@@ -9,15 +10,18 @@ class TransactionListView(ListView):
     context_object_name = 'transactions'
     
     def get_queryset(self):
+        # ADD: show transactions only for specific user
         return Transaction.objects.all().order_by('-date')[:50]  # last 50 transactions
 
-class TransactionCreateView(CreateView):
+class TransactionCreateView(LoginRequiredMixin, CreateView):
     model = Transaction
     form_class = TransactionForm
     template_name = 'transactions/transaction_form.html'
+    success_url = '/transactions/'
     
     def form_valid(self, form):
-        # Should be real user binding in production
+        # Bind transaction to current user
+        form.instance.user = self.request.user
         return super().form_valid(form)
 
 def index(request):
